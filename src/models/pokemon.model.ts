bc2dc64cb60785d0b7036555ad0493c9aa01b4e3
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-01-30 18:22:51 -0300

    ✨ feat: pokemon model class

---
+++ src/models/pokemon.model.ts
@@ -0,0 +1,54 @@
+import { Type } from "./type.model";
+
+export class Pokemon {
+  name: string;
+  type: [Type, Type?];
+  image: Image;
+  hp: HP;
+  attack: number;
+  defense: number;
+  spAttack: number;
+  spDefense: number;
+  speed: number;
+  attacks: [Attack, Attack, Attack, Attack];
+  lastPercentDamaged: number;
+
+  applyDamage(attackerPokemon: Pokemon): void {
+    const damage = attackerPokemon.calculateDamage(this);
+    const percentageDamaged = (damage / this.hp.current) * 100;
+    this.hp.current -= damage;
+    this.lastPercentDamaged = percentageDamaged;
+  }
+
+  calculateDamage(defensorPokemon: Pokemon) {
+    //Damage = (0.5 X Power X Attack / Defense X Modifier) + 1
+    //Damage = (0.5 X Power X SpAttack / SpDefense X Modifier) + 1
+    return 300;
+  }
+}
+
+export interface Attack {
+  name: string;
+  type: Type;
+  category: Category;
+  power: number;
+  accuracy: number;
+  PowerPoints: number;
+}
+
+export interface Image {
+  default: string;
+  front: string;
+  back: string;
+}
+
+export type Progress = {
+  current: number;
+  total: number;
+};
+
+export interface HP extends Progress {}
+
+export interface PP extends Progress {}
+
+export type Category = "Special" | "Physical";
